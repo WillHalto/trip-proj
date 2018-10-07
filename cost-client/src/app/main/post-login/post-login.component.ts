60db71ff27c1448179ba14f1a48d2dac0e073215
Joshua Yang <yko06300@gmail.com>
2018-10-07 16:16:28 -0500

    clean up

--- cost-client/src/app/main/post-login/post-login.component.ts
+++ cost-client/src/app/main/post-login/post-login.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-post-login',
   templateUrl: './post-login.component.html',
   styleUrls: ['./post-login.component.css']
 })
 export class PostLoginComponent implements OnInit {
-  constructor(public authService: AuthService, private meta: Meta) {
+  constructor(public authService: AuthService) {
    }
 
   ngOnInit() {   
